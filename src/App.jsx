7016cac17eeca41307f5b6d8d37acc345cf95db3
Ivan Bondaruk <bondarukid@icloud.com>
2025-08-15 23:21:46 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
