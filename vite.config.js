043bc821704bb1274171b8f0fb46c254ae892324
Ivan Bondaruk <bondarukid@icloud.com>
2025-08-21 14:03:55 +0200

    SItemap

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-
+import sitemap from 'vite-plugin-sitemap';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),
+      tailwindcss(),
+      sitemap({
+          hostname: 'https://bondarukid.com', // ваш домен
+          routes: [
+              '/', '/software/ohmslaw', '/software/ohmslaw/policy' // список страниц (можно динамически)
+          ]
+      })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
